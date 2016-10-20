562f9a4181044ada07c3d3b327055658300bf198
Jason Foster <c-jason.foster@skykick.com>
2016-10-20 15:50:43 -0600

    Spell check and further references

--- PartOne.R
+++ PartOne.R
@@ -1,59 +1,97 @@
-library(ggplot2)
-library(gridExtra)
-library(matrixStats)
-
-windowsFonts(Avenir = windowsFont("Avenir Medium"))
+suppressMessages(library(ggplot2))
+suppressMessages(library(gridExtra))
+suppressMessages(library(dplyr))
 
 ex1 <- data.frame(val = rexp(1000, 1.33))
 ex1Plot <-  ggplot(ex1, aes(x=val)) + 
   geom_density(col = "black", fill = "cornflowerblue", alpha = .25) +
   ggtitle(expression(paste("Exponential distribution with ", lambda, " = 1.33"))) +
   labs(x = "X", y = "Density") +
-  theme_bw(base_family = "Avenir", base_size = 12)
+  theme_bw(base_size = 10)
 
-ex2 <- data.frame(val = rexp(1000, 1))
+ex2 <- data.frame(val = rexp(1000, .5))
 ex2Plot <-  ggplot(ex2, aes(x=val)) + 
-  geom_density(col = "black", fill = "cornflowerblue", alpha = .25) +
-  ggtitle(expression(paste("Exponential distribution with ", lambda, " = 1.00"))) +
-  labs(x = "X", y = "Density") +
-  theme_bw(base_family = "Avenir", base_size = 12)
-
-ex3 <- data.frame(val = rexp(1000, .80))
-ex3Plot <-  ggplot(ex3, aes(x=val)) + 
-  geom_density(col = "black", fill = "cornflowerblue", alpha = .25) +
-  ggtitle(expression(paste("Exponential distribution with ", lambda, " = 0.80"))) +
-  labs(x = "X", y = "Density") +
-  theme_bw(base_family = "Avenir", base_size = 12)
-
-ex4 <- data.frame(val = rexp(1000, .67))
-ex4Plot <-  ggplot(ex4, aes(x=val)) + 
   geom_density(col = "black", fill = "cornflowerblue", alpha = .25) +
   ggtitle(expression(paste("Exponential distribution with ", lambda, " = 0.67"))) +
   labs(x = "X", y = "Density") +
-  theme_bw(base_family = "Avenir", base_size = 12)
+  theme_bw(base_size = 10)
 
-grid.arrange(ex1Plot, ex2Plot, ex3Plot, ex4Plot)
+grid.arrange(ex1Plot, ex2Plot, nrow=1, ncol=2)
 
-simulations <- data.frame(t(replicate(1000, rexp(40, .2)))) %>%
-  mutate(mean = rowMeans(.), flux = sqrt(40) * (mean - 5))
 
-ggplot(simulations, aes(x=mean)) +
+lambda <- .2
+n <- 40
+numSims <- 1000
+mu <- 1 / lambda
+simulations <- data.frame(t(replicate(numSims, rexp(n, lambda)))) %>%
+  mutate(xBar = rowMeans(.), flux = sqrt(n) * (xBar - mu))
+ggplot(simulations, aes(x=xBar)) +
   ggtitle("Histogram of Sample Means\n") +
-  labs(x = "Mean of 40 Random Exponentials", y = "Frequency") +
-  geom_histogram(col="black", fill="cornflowerblue", alpha=.5, binwidth=.25) +
-  geom_vline(aes(xintercept = mean(mean), color = "Theoretical"), show.legend = TRUE) +
-  scale_colour_manual(name = "Legend", values = c(Theoretical = "firebrick")) +
-  theme_bw(base_family = "Avenir", base_size = 12)
+  labs(x = "Means of 40 Random Exponentials", y = "Frequency") +
+  geom_histogram(aes(y = ..density..), col = "azure4", fill = "cornflowerblue", 
+                 alpha = .5, binwidth = .25) +
+  geom_density(color = "chartreuse4", size = 1) +
+  geom_vline(aes(xintercept = mean(xBar), color = "firebrick"), 
+             show.legend = FALSE) +
+  theme_bw(base_size = 10)
 
-sampleMean <- mean(simulations$mean)
 
-ggplot(simulations, aes(x=var)) +
-  ggtitle("Histogram of Sample Variances\n") +
-  labs(x = "Variances of 40 Random Exponentials", y = "Frequency") +
-  geom_histogram(col="black", fill="cornflowerblue", alpha=.5, binwidth=5) +
-  geom_vline(aes(xintercept = mean(var), color = "Sample"), show.legend = TRUE) +
-  scale_colour_manual(name = "Legend", values = c(Sample = "firebrick")) +
-  theme_bw(base_family = "Avenir", base_size = 12)
+confInt <- t.test(simulations$xBar)$conf.int
+sampleMean <- mean(simulations$xBar)
+data.frame(Mean = c(sampleMean, mu), row.names = c("Sample", "Theoretical"))
+lowerCI <- confInt[1]
+upperCI <- confInt[2]
 
-var(simulations$mean)
 
+sigmaSq <- (1 / (lambda^2)) / n
+sampleVariance <- var(simulations$xBar)
+data.frame(Variance = c(sampleVariance, sigmaSq), 
+           row.names = c("Sample", "Theoretical"))
+
+
+ggplot(simulations, aes(x=flux)) +
+  ggtitle("Histogram of Fluctuation\n") +
+  labs(x = "Fluctuation of Mean of 40 Random Exponentials", y = "Frequency") +
+  geom_histogram(aes(y = ..density..), col = "azure4", fill = "cornflowerblue",
+                 alpha = .5, binwidth = 1) +
+  geom_density(color = "chartreuse4", size = 1) +
+  stat_function(fun = dnorm, color = "darkorange", size = 1, 
+                args = list(mean = 0, sd = 5)) +
+  geom_vline(aes(xintercept = mean(flux), color = "firebrick"), 
+             show.legend = FALSE) +
+  theme_bw(base_size = 10)
+
+
+fluxMean <- mean(simulations$flux)
+fluxSd <- sd(simulations$flux)
+data.frame(Value = c(fluxMean, fluxSd), 
+           row.names = c("Mean", "Standard Deviation"))
+
+
+bigSim <- rexp(10000, .2)
+par(mfrow=c(1,2))
+qqnorm(y = simulations$xBar, col = "darkslateblue", 
+       main = "Q-Q Plot of Sample Means")
+qqline(y = simulations$xBar, col = "firebrick1")
+qqnorm(y = bigSim, col = "darkslateblue", 
+       main = "Q-Q Plot of Exp. Distribution")
+qqline(y = bigSim, col = "firebrick1")
+
+
+ex1 <- data.frame(val = rexp(1000, 1.33))
+ex1Plot <-  ggplot(ex1, aes(x=val)) + 
+  geom_density(col = "black", fill = "cornflowerblue", alpha = .25) +
+  ggtitle(expression(paste("Exponential distribution with ", 
+                           lambda, " = 1.33"))) +
+  labs(x = "X", y = "Density") +
+  theme_bw(base_size = 10)
+
+ex2 <- data.frame(val = rexp(1000, .5))
+ex2Plot <-  ggplot(ex2, aes(x=val)) + 
+  geom_density(col = "black", fill = "cornflowerblue", alpha = .25) +
+  ggtitle(expression(paste("Exponential distribution with ", 
+                           lambda, " = 0.67"))) +
+  labs(x = "X", y = "Density") +
+  theme_bw(base_size = 10)
+
+grid.arrange(ex1Plot, ex2Plot, nrow=1, ncol=2)
--- PartTwo.R
+++ PartTwo.R
@@ -1,32 +1,15 @@
-library(datasets)
-library(ggplot2)
-library(dplyr)
-
+suppressMessages(library(datasets))
+suppressMessages(library(ggplot2))
+suppressMessages(library(dplyr))
+suppressMessages(library(knitr))
 data("ToothGrowth")
 
-smDose <- ToothGrowth %>%
-  filter(dose == .5)
 
-mdDose <- ToothGrowth %>%
-  filter(dose == 1.0)
-
-lgDose <- ToothGrowth %>%
-  filter(dose == 2.0)
-
-ojData <- ToothGrowth %>%
-  filter(supp == 'OJ')
-#  group_by(factor(dose)) %>%
-#  summarize("mean" = mean(len), "variance" = var(len), "sd" = sd(len))
-
-stdData <- ToothGrowth %>%
-  filter(supp == 'VC')
-#  group_by(factor(dose)) %>%
-#  summarize("mean" = mean(len), "variance" = var(len), "sd" = sd(len))
-
-ojT <- t.test(ToothGrowth$len)
+str(ToothGrowth)
+paste("Complete Cases:", sum(complete.cases(ToothGrowth)))
 
 
-ojPlot <- ToothGrowth %>%
+ToothGrowth %>%
   ggplot(aes(x=factor(dose), y=len, group=supp, color=supp)) +
   ggtitle("Odontoblast Length as a Function of Dose\n") +
   labs(x = "Dosage (mg)", y = "Odontoblasts Length (microns)", color="Supplement") +
@@ -34,29 +17,55 @@ ojPlot <- ToothGrowth %>%
   stat_summary(aes(group=supp, color=supp), fun.y=mean, geom="line") +
   theme_bw(base_size = 10)
 
-stdPlot <- ToothGrowth %>%
-  filter(supp == 'VC') %>%
-  ggplot(aes(factor(dose), len)) +
-  geom_violin()
-  #stat_summary(fun.y=mean, geom="line", aes(group=1))
 
-grid.arrange(ojPlot, stdPlot)
-
-
-ggplot(ToothGrowth, aes(x=len)) + 
-  geom_histogram(aes(y = ..density..), col = "azure4", fill = "cornflowerblue", alpha = .5, binwidth = 2) +
-  geom_density(col = "black", fill = "cornflowerblue", alpha = .25)
-
-data.frame(
-  "Orange Juice",
-  "Ascorbic Acid",
-  paste("[", round(t4$conf.int[1],4), ", ", round(t4$conf.int[2],4), "]", sep=""),
-  round(unname(t4$parameter), 2),
-  qt(.975, unname(t4$parameter)),
-  4,
-  5)
+t1 <- t.test(sOJ$len, sVC$len, conf.level=.975, paired=FALSE, var.equal=FALSE)
+kable(
+  data.frame(
+    "Ascorbic Acid", 
+    "Orange Juice", 
+    paste("[", round(t1$conf.int[1],4), ", ", round(t1$conf.int[2],4), "]", sep=""), 
+    round(unname(t1$parameter), 2),  
+    qt(.9875, unname(t1$parameter)), 
+    unname(t1$statistic), t1$p.value), 
+  col.names = c("Group 1 Supp", "Group 2 Supp", "Conf Int", 
+                "Deg F", "Tabulated t-value", "t-statistic", "p-value"))
 
 
+t2 <- t.test(d1$len, d05$len, conf.level=.975, paired=FALSE, var.equal=FALSE)
+t3 <- t.test(d2$len, d05$len, conf.level=.975, paired=FALSE, var.equal=FALSE)
+t4 <- t.test(d2$len, d1$len, conf.level=.975, paired=FALSE, var.equal=FALSE)
+g1dose <- c("1", "2", "2")
+g2dose <- c(".5", ".5", "1")
+ci <- c(paste("[", round(t2$conf.int[1], 4), ", ", round(t2$conf.int[2], 4), "]", sep=""), 
+        paste("[", round(t3$conf.int[1], 4), ", ", round(t3$conf.int[2], 4), "]", sep=""), 
+        paste("[", round(t4$conf.int[1], 4), ", ", round(t4$conf.int[2], 4), "]", sep=""))
+df <- c(unname(t2$parameter),
+        unname(t3$parameter),
+        unname(t4$parameter))
+tT <- c(qt(.9875, unname(t2$parameter)),
+        qt(.9875, unname(t3$parameter)),
+        qt(.9875, unname(t4$parameter)))
+cT <- c(unname(t2$statistic),
+        unname(t3$statistic),
+        unname(t4$statistic))
+pval <- c(t2$p.value, t3$p.value, t4$p.value)
+kable(data.frame(g1dose, g2dose, ci, df, tT, cT, pval), 
+      col.names = c("Group 1 Dose","Group 2 Dose","Conf Int", "Deg F", 
+                    "Tabulated t-value", "t-statistic", "p-value"))
 
 
+allP <- c(t1$p.value, t2$p.value, t3$p.value, t4$p.value)
+g1 <- c("Ascorbic Acid", "1", "2", "2")
+g2 <- c("Orange Juice", ".5", ".5", "1")
+adjP <- p.adjust(allP, method="bonferroni")
+origConclusion <- allP > .025
+adjConclusion <- adjP > .025
+kable(data.frame(g1, g2, allP, origConclusion, adjP, adjConclusion), 
+      col.names = c("Group1", "Group 2", "Original p-value", "> alpha?", 
 
+                    
+                    adjP <- p.adjust(allP, method="BH")
+adjConclusion <- adjP > .025
+kable(data.frame(g1, g2, allP, origConclusion, adjP, adjConclusion), 
+      col.names = c("Group1", "Group 2", "Original p-value", "> alpha?", 
+                    "Adjusted p-value", "> alpha?"))
